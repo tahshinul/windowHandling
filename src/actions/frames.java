d30d4fd0f0dcb1038b8d784344f961d4a0e49245
Tahshinul Islam <tahshin123@gmail.com>
2024-02-24 18:14:06 +0600

    handlingframes

---
+++ src/actions/frames.java
@@ -0,0 +1,30 @@
+package actions;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+
+public class frames {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get("http://jqueryui.com/droppable/");
+		driver.switchTo().frame(driver.findElement(By.xpath("//iframe[@class='demo-frame']")));
+		
+		Actions a = new Actions(driver);
+		a.dragAndDrop(driver.findElement(By.id("draggable")), driver.findElement(By.id("droppable"))).build().perform();
+		
+		
+		
+//		driver.findElement(By.id("draggable")).click(); 
+//		driver.quit();
+
+	}
+
+}
